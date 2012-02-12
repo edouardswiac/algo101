98f85d193a1e8855040e1d82e22c167d2dec74b0
Edouard Swiac <edouard.swiac@gmail.com>
2012-02-11 19:52:24 -0800

    make insertion_sort a module

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,23 +1,25 @@
 ## INSERTION SORT
 ## Edouard Swiac
 
+module InsertionSort
+  def self.sort(input)
+    for index in 0...input.size
+      self.insert(input, index, input[index])
+    end
+  end
+  
+  private
+  def self.insert(input, index, value)
+    i = index - 1
+    while i >= 0 and input[i] > value
+      input[i + 1] = input[i]
+      i = i - 1
+    end
+    input[i + 1] = value
+  end
+end
+
 input = 25.times.map { Random.rand(500) }
-
-def sort(input)
-  for index in 0...input.size
-    insert(input, index, input[index])
-  end
-end
-
-def insert(input, index, value)
-  i = index - 1
-  while i >= 0 and input[i] > value
-    input[i + 1] = input[i]
-    i = i - 1
-  end
-  input[i + 1] = value
-end
-
 puts "Insertion sort for the following input #{input}"
-sort(input)
+InsertionSort.sort(input)
 puts "Input sorted #{input}"
